17115a3f144a12b561df73f8aa77e2a7f0ba344d
Erick Giancarlo <zairo2542@gmail.com>
2020-06-10 21:08:33 -0500

    10-6-20

--- dev/js/includes/searchTask.js
+++ dev/js/includes/searchTask.js
@@ -1,30 +1,36 @@
-import { renderTask } from "../taskControl/readTask";
+import {renderTask} from '../taskControl/readTask';
 
 //No eliminar
-import { async } from "../generatorRunTime";
+import {async} from '../generatorRunTime';
 
 const searchTask = () => {
-    let $search = document.querySelector("#searchTask");
+	let $search = document.querySelector('#searchTask');
+	let $close = document.querySelector('#modal-close');
+	$close.addEventListener('click', () => {
+		$search.reset();
+	});
 
-    $search.addEventListener("submit", async(e) => {
+	$search.addEventListener('submit', async (e) => {
+		//Preparamos los datos del Formulario para enviarlo
+		let data = new FormData($search);
+		e.preventDefault();
 
-        //Preparamos los datos del Formulario para enviarlo
-        let data = new FormData($search);
-        e.preventDefault();
+		if (data.get('search')) {
+			//Enviamos los datos
+			let options = {
+				method: 'POST',
+				body: data,
+			};
 
+			let res = await fetch('./php/searchTask.php', options);
+			let respuesta = await res.json();
 
-        //Enviamos los datos
-        let options = {
-            method: "POST",
-            body: data
-        };
-
-        let res = await fetch("./php/searchTask.php", options);
-        let respuesta = await res.json();
-
-        //Renderizamos el resultado en el modal
-        renderTask(respuesta, "#modal")
-    });
-}
+			//Renderizamos el resultado en el modal
+			renderTask(respuesta, '#modal');
+		} else {
+			renderTask({}, '#modal');
+		}
+	});
+};
 
 export default searchTask;
--- dev/js/taskControl/readTask.js
+++ dev/js/taskControl/readTask.js
@@ -1,34 +1,31 @@
-import deleteTask from "./deleteTask";
-import updateTask from "./updateTask";
+import deleteTask from './deleteTask';
+import updateTask from './updateTask';
 
 //Importante no eliminar
-import { async } from "../generatorRunTime";
+import {async} from '../generatorRunTime';
 
+const readTask = async () => {
+	let options = {
+		method: 'GET',
+	};
 
-const readTask = async() => {
-    let options = {
-        method: "GET"
-    };
+	let res = await fetch('./php/getTable.php', options);
+	let data = await res.json();
 
-    let res = await fetch("./php/getTable.php", options);
-    let data = await res.json();
-
-    renderTask(data, "#table");
-    renderTask(data, "#modal");
-
+	renderTask(data, '#table');
+	renderTask(data, '#modal');
 };
 
-
 export const renderTask = (props, element) => {
-    let $tableTask = document.querySelector(element);
+	let $tableTask = document.querySelector(element);
 
-    //Reinicia la tabla para actualizarlas
-    $tableTask.innerHTML = "";
+	//Reinicia la tabla para actualizarlas
+	$tableTask.innerHTML = '';
 
-    //Cuerpo de la tabla
-    if (props.length) {
-        props.forEach(element => {
-            $tableTask.innerHTML += `
+	//Cuerpo de la tabla
+	if (props.length) {
+		props.forEach((element) => {
+			$tableTask.innerHTML += `
             <tr>
                 <td class="text-wrap">${element.id}</td>
                 <td class="text-wrap">${element.title}</td>
@@ -46,39 +43,34 @@ export const renderTask = (props, element) => {
                 </td>
             </tr>
             `;
-        });
-
-    } else {
-        $tableTask.innerHTML = `
+		});
+	} else {
+		$tableTask.innerHTML = `
         <tr>
             <td colspan="4" class="text-center">
             Empty to do list
             </td>
         </tr>
         `;
-    }
-    //Añadimos la funcionalidad de eliminar
-    deleteTask();
-    //Añadimos la funcionalidad de actualizar
-    updateTask();
-
-}
+	}
+	//Añadimos la funcionalidad de eliminar
+	deleteTask();
+	//Añadimos la funcionalidad de actualizar
+	updateTask();
+};
 
 let icons = {
-    edit: `
+	edit: `
         <svg class="bi bi-bookmark-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M4.5 2a.5.5 0 0 0-.5.5v11.066l4-2.667 4 2.667V8.5a.5.5 0 0 1 1 0v6.934l-5-3.333-5 3.333V2.5A1.5 1.5 0 0 1 4.5 1h4a.5.5 0 0 1 0 1h-4zm9-1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H13V1.5a.5.5 0 0 1 .5-.5z"/>
         <path fill-rule="evenodd" d="M13 3.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0v-2z"/>
         </svg>
     `,
-    delete: `
+	delete: `
         <svg class="bi bi-trash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
         </svg>
-    `
-}
-
-
-
+    `,
+};
 
 export default readTask;
